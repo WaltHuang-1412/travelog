e2cf5d0dd7bc227d63f14882a01450054915ce7d
Walter1412 <walt.huang.1412@gmail.com>
2022-07-08 01:25:30 +0800

    update layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,31 @@
 <template lang="pug">
 v-app
   v-navigation-drawer(app)
-  v-main(app)
-    slot
-  v-footer(app)
+    template(v-slot:prepend)
+      v-list-item(two-line='' prepend-avatar='https://randomuser.me/api/portraits/women/81.jpg' title='Jane Smith' subtitle='Logged in')
+    v-divider
+    v-list(density='compact')
+      v-list-item(prepend-icon='mdi-home-city' title='Home' value='home')
+      v-list-item(prepend-icon='mdi-account' title='My Account' value='account')
+      v-list-item(prepend-icon='mdi-account-group-outline' title='Users' value='users')
+
+    template(v-slot:append)
+      .pa-2
+        v-btn(block)
+          | Logout
+
+  v-app-bar(app,color="teal-darken-4",image="https://picsum.photos/1920/1080?random")
+    template(v-slot:image)
+      v-img(gradient='to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)')
+    template(v-slot:prepend)
+      v-app-bar-nav-icon
+    v-spacer
+    v-btn(icon)
+      v-icon mdi-login
+
+  v-main
+    v-container(fluid)
+      router-view
 
 </template>
 
@@ -13,6 +35,12 @@ import { defineComponent } from 'vue'
 // Component
 
 export default defineComponent({
-  name: 'LayoutDefault'
+  name: 'LayoutDefault',
+  setup() {
+    const drawer = null
+    return {
+      drawer
+    }
+  }
 })
 </script>
